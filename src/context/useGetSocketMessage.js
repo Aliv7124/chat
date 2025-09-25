bedcf5eca3102bcf024bdfcab05b8366498c1bdd
Aliv7124 <alivroy25@gmail.com>
2025-09-25 17:37:08 +0530

    Fix msg

--- src/context/useGetSocketMessage.js
+++ src/context/useGetSocketMessage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+/* import { useEffect } from "react";
 import { useSocketContext } from "./SocketContext";
 import useConversation from "../zustand/useConversation";
 
@@ -15,6 +15,36 @@ const useGetSocketMessage = () => {
     });
     return () => socket.off("newMessage");
   }, [socket, messages]);
+};
+
+export default useGetSocketMessage;
+*/
+
+
+
+import { useEffect } from "react";
+import { useSocketContext } from "./SocketContext";
+import useConversation from "../zustand/useConversation";
+
+const useGetSocketMessage = () => {
+  const { socket } = useSocketContext();
+  const { selectedConversation, addMessage } = useConversation();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("receive_message", (newMessage) => {
+      const audio = new Audio("/assets/notification.mp3");
+      audio.play();
+
+      if (selectedConversation && newMessage.from === selectedConversation._id) {
+        // add message only to the active conversation
+        addMessage(selectedConversation._id, newMessage);
+      }
+    });
+
+    return () => socket.off("receive_message");
+  }, [socket, selectedConversation, addMessage]);
 };
 
 export default useGetSocketMessage;
